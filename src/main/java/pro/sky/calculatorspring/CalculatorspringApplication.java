1d969df451dc83379a58c64d2dbd8cf70eba984a
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-10-24 18:44:41 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculatorspring/CalculatorspringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculatorspring/CalculatorspringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

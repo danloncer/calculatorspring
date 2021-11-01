af1b7980a7b608ab1a2af7579d3775246d16592b
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-11-01 18:45:14 +0300

    Добби почти свободен..

--- src/main/java/pro/sky/calculatorspring/CalculatorService.java
+++ src/main/java/pro/sky/calculatorspring/CalculatorService.java
@@ -1,8 +1,8 @@
 package pro.sky.calculatorspring;
 
 public interface CalculatorService {
-    Integer findSum(int num1, int num2);
-    Integer findDifference(int num1, int num2);
-    Integer findMultiply(int num1, int num2);
-    Integer findDivide(int num1, int num2);
+    int findSum(int num1, int num2);
+    int findDifference(int num1, int num2);
+    int findMultiply(int num1, int num2);
+    int findDivide(int num1, int num2);
 }
--- src/main/java/pro/sky/calculatorspring/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculatorspring/CalculatorServiceImpl.java
@@ -4,19 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-    public Integer findSum(int num1, int num2) {
+    public int findSum(int num1, int num2) {
         return (num1 + num2);
     }
 
-    public Integer findDifference(int num1, int num2) {
+    public int findDifference(int num1, int num2) {
         return (num1 - num2);
     }
 
-    public Integer findMultiply(int num1, int num2) {
+    public int findMultiply(int num1, int num2) {
         return (num1 * num2);
     }
 
-    public Integer findDivide(int num1, int num2) {
+    public int findDivide(int num1, int num2) {
         return (num1/num2);
     }
 }
